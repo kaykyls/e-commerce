bf5d58ef070bbe1f76c6b0b458f5c49c9b6bb5c8
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-05-21 00:07:05 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -48,9 +48,14 @@ function App() {
                     <ul className="dropdown-menu">
                       <li><a className="dropdown-item" href="#">category 1</a></li>
                       <li><a className="dropdown-item" href="#">category 2</a></li>
-                      <li><hr className="dropdown-divider"/></li>
                       <li><a className="dropdown-item" href="#">category 3</a></li>
                     </ul>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">Offers</a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="#" className="nav-link">Sell</a>
                   </li>
                 </ul>
               </div>
@@ -59,8 +64,19 @@ function App() {
           </div>
         </div>
 
-        <div className="slider-container mt-5">
-          <div className="carousel slide carousel-dark" data-bs-ride="carousel">
+        <div className="info mt-5">
+          <div className="h3">
+            <h3>Recommended</h3>
+          </div>
+        </div>
+
+        <div className="slider-container">
+          <div id='recommended-carousel' className="carousel slide carousel-dark" data-bs-ride="carousel">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#recommended-carousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#recommended-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#recommended-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
             <div className="carousel-inner">
               <div className="carousel-item active" data-bs-interval="5000">
                 <img src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="d-block w-100" alt="..."/>
@@ -76,19 +92,19 @@ function App() {
                   <p>Just some nice shoe passing through.</p>
                 </div>
               </div>
-              <div className="carousel-item">
+              <div className="carousel-item" data-bs-interval="5000">
                 <img src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80" className="d-block w-100" alt="..."/>
-                <div className="carousel-caption d-none d-md-block" data-bs-interval="5000">
+                <div className="carousel-caption d-none d-md-block">
                   <h5>This is a shoe</h5>
                   <p>Just some nice shoe passing through.</p>
                 </div>
               </div>
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <button className="carousel-control-prev" type="button" data-bs-target="#recommended-carousel" data-bs-slide="prev">
               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
               <span className="visually-hidden">Previous</span>
             </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <button className="carousel-control-next" type="button" data-bs-target="#recommended-carousel" data-bs-slide="next">
               <span className="carousel-control-next-icon" aria-hidden="true"></span>
               <span className="visually-hidden">Next</span>
             </button>
