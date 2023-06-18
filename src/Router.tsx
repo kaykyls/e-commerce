955aafc0fc75ac1e79b78f7966c1c07c50d6f290
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-06-17 23:47:33 -0300

    add login page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Cart from './pages/cart/Cart';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import Product from './pages/product/Product';
 import Register from './pages/register/Register';
 
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/product/:id" element={<Product/>}/>
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
   )
 }
